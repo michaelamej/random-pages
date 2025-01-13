2169c07fb558888e9f8f7045a8d9e1a06ba297ee
Michaela Presser <147770729+michaelamej@users.noreply.github.com>
2025-01-13 09:41:34 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,126 @@
+<footer class="wp-block-template-part">
+					<div class="wp-block-group alignfull footer-dark is-layout-constrained wp-block-group-is-layout-constrained">
+					<div class="wp-block-group alignfull wp-block-footer wp-block-widget-area is-layout-constrained wp-block-group-is-layout-constrained">
+					<div class="wp-block-group alignwide is-layout-flow wp-block-group-is-layout-flow">
+					<div class="wp-block-columns wp-block-footer-widget-area-columns is-layout-flex wp-container-core-columns-is-layout-1 wp-block-columns-is-layout-flex">
+					<div class="wp-block-column wp-block-widget is-layout-flow wp-block-column-is-layout-flow">
+					<div class="wp-block-group is-layout-flow wp-block-group-is-layout-flow">
+					<h2 class="wp-block-heading" id="about" style="margin-top:0px;margin-right:0px;margin-bottom:20px;margin-left:0px">About</h2>
+					
+
+					
+					<p>Lorem ipsum dolor sit amet con sect etur adipiscing elit maecenas conse ctetur dapibus rutrum proin vitae nibh eget adipiscing elit maecenas cons.</p>
+					</div>
+					
+					<div class="wp-block-group is-layout-flow wp-block-group-is-layout-flow">
+					<ul class="wp-block-social-links has-icon-color is-style-logos-only is-layout-flex wp-block-social-links-is-layout-flex"><li style="color: #ffffff; " class="wp-social-link wp-social-link-facebook has-foreground-color wp-block-social-link"><a href="https://#" class="wp-block-social-link-anchor"><svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M12 2C6.5 2 2 6.5 2 12c0 5 3.7 9.1 8.4 9.9v-7H7.9V12h2.5V9.8c0-2.5 1.5-3.9 3.8-3.9 1.1 0 2.2.2 2.2.2v2.5h-1.3c-1.2 0-1.6.8-1.6 1.6V12h2.8l-.4 2.9h-2.3v7C18.3 21.1 22 17 22 12c0-5.5-4.5-10-10-10z"></path></svg><span class="wp-block-social-link-label screen-reader-text">Facebook</span></a></li>
+
+					<li style="color: #ffffff; " class="wp-social-link wp-social-link-twitter has-foreground-color wp-block-social-link"><a href="https://#" class="wp-block-social-link-anchor"><svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M22.23,5.924c-0.736,0.326-1.527,0.547-2.357,0.646c0.847-0.508,1.498-1.312,1.804-2.27 c-0.793,0.47-1.671,0.812-2.606,0.996C18.324,4.498,17.257,4,16.077,4c-2.266,0-4.103,1.837-4.103,4.103 c0,0.322,0.036,0.635,0.106,0.935C8.67,8.867,5.647,7.234,3.623,4.751C3.27,5.357,3.067,6.062,3.067,6.814 c0,1.424,0.724,2.679,1.825,3.415c-0.673-0.021-1.305-0.206-1.859-0.513c0,0.017,0,0.034,0,0.052c0,1.988,1.414,3.647,3.292,4.023 c-0.344,0.094-0.707,0.144-1.081,0.144c-0.264,0-0.521-0.026-0.772-0.074c0.522,1.63,2.038,2.816,3.833,2.85 c-1.404,1.1-3.174,1.756-5.096,1.756c-0.331,0-0.658-0.019-0.979-0.057c1.816,1.164,3.973,1.843,6.29,1.843 c7.547,0,11.675-6.252,11.675-11.675c0-0.178-0.004-0.355-0.012-0.531C20.985,7.47,21.68,6.747,22.23,5.924z"></path></svg><span class="wp-block-social-link-label screen-reader-text">Twitter</span></a></li>
+
+					<li style="color: #ffffff; " class="wp-social-link wp-social-link-linkedin has-foreground-color wp-block-social-link"><a href="https://#" class="wp-block-social-link-anchor"><svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M19.7,3H4.3C3.582,3,3,3.582,3,4.3v15.4C3,20.418,3.582,21,4.3,21h15.4c0.718,0,1.3-0.582,1.3-1.3V4.3 C21,3.582,20.418,3,19.7,3z M8.339,18.338H5.667v-8.59h2.672V18.338z M7.004,8.574c-0.857,0-1.549-0.694-1.549-1.548 c0-0.855,0.691-1.548,1.549-1.548c0.854,0,1.547,0.694,1.547,1.548C8.551,7.881,7.858,8.574,7.004,8.574z M18.339,18.338h-2.669 v-4.177c0-0.996-0.017-2.278-1.387-2.278c-1.389,0-1.601,1.086-1.601,2.206v4.249h-2.667v-8.59h2.559v1.174h0.037 c0.356-0.675,1.227-1.387,2.526-1.387c2.703,0,3.203,1.779,3.203,4.092V18.338z"></path></svg><span class="wp-block-social-link-label screen-reader-text">LinkedIn</span></a></li></ul>
+					</div>
+					</div>
+					
+
+					
+					<div class="wp-block-column wp-block-widget is-layout-flow wp-block-column-is-layout-flow">
+						
+						<div class="wp-block-group is-layout-flow wp-block-group-is-layout-flow">
+							
+							<h2 class="wp-block-heading" id="contact" style="margin-top:0px;margin-right:0px;margin-bottom:20px;margin-left:0px">Contact</h2>
+							
+
+							
+							<div class="wp-block-group is-layout-flex wp-block-group-is-layout-flex">
+								
+								<figure class="wp-block-image size-full"><img src="https://mpressivewebdv.com/wpp/wp-content/themes/catch-fse/assets/images/icon-location.png" alt="" class=""></figure>
+								
+
+								
+								<p><a href="#">374 FA Tower, Williams S Blvd. 2721, IL, USA</a></p>
+								
+							</div>
+							
+
+							
+							<div class="wp-block-group is-layout-flex wp-block-group-is-layout-flex">
+								
+								<figure class="wp-block-image size-full"><img src="https://mpressivewebdv.com/wpp/wp-content/themes/catch-fse/assets/images/icon-phone.png" alt="" class=""></figure>
+								
+
+								
+								<p><a href="#">(+880) 123 3456 7890</a></p>
+								
+							</div>
+							
+
+							
+							<div class="wp-block-group is-layout-flex wp-block-group-is-layout-flex">
+								
+								<figure class="wp-block-image size-full"><img src="https://mpressivewebdv.com/wpp/wp-content/themes/catch-fse/assets/images/icon-envelope.png" alt="" class=""></figure>
+								
+
+								
+								<p><a href="#">info@companyname.com</a></p>
+								
+							</div>
+							
+
+							
+							<div class="wp-block-group is-layout-flex wp-block-group-is-layout-flex">
+								
+								<figure class="wp-block-image size-full"><img src="https://mpressivewebdv.com/wpp/wp-content/themes/catch-fse/assets/images/icon-clock.png" alt="" class=""></figure>
+								
+
+								
+								<p><a href="#">Opening Hours: 10:00 18:00</a></p>
+								
+							</div>
+							
+						</div>
+						
+					</div>
+					
+
+					
+					<div class="wp-block-column wp-block-widget is-layout-flow wp-block-column-is-layout-flow">
+					<div class="wp-block-group is-layout-flow wp-block-group-is-layout-flow">
+					<h2 class="wp-block-heading" id="recent-post" style="margin-top:0px;margin-right:0px;margin-bottom:20px;margin-left:0px">Recent Post</h2>
+					
+
+					<ul class="wp-block-latest-posts__list has-dates wp-block-latest-posts"><li><a class="wp-block-latest-posts__post-title" href="https://mpressivewebdv.com/wpp/2025/01/10/hello-world/">Hello world!</a><time datetime="2025-01-10T18:09:58+00:00" class="wp-block-latest-posts__post-date">January 10, 2025</time></li>
+</ul></div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					
+
+					<div class="wp-block-group alignfull footer-dark is-layout-constrained wp-block-group-is-layout-constrained">
+						
+						<div class="wp-block-group alignfull wp-block-footer  wp-block-site-generator is-layout-constrained wp-block-group-is-layout-constrained">
+							
+							<div class="wp-block-group alignwide wp-block-site-info is-content-justification-space-between is-layout-flex wp-container-core-group-is-layout-21 wp-block-group-is-layout-flex">
+								
+								<p> Copyright © 2025. Powered by <a href="https://catchthemes.com" rel="nofollow">Catch Themes</a></p>
+								
+
+								<nav class="is-responsive items-justified-right wp-block-navigation is-content-justification-right is-layout-flex wp-container-core-navigation-is-layout-2 wp-block-navigation-is-layout-flex" aria-label="" data-wp-interactive="core/navigation" data-wp-context="{&quot;overlayOpenedBy&quot;:{&quot;click&quot;:false,&quot;hover&quot;:false,&quot;focus&quot;:false},&quot;type&quot;:&quot;overlay&quot;,&quot;roleAttribute&quot;:&quot;&quot;,&quot;ariaLabel&quot;:&quot;Menu&quot;}"><button aria-haspopup="dialog" aria-label="Open menu" class="wp-block-navigation__responsive-container-open " data-wp-on-async--click="actions.openMenuOnClick" data-wp-on--keydown="actions.handleMenuKeydown"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><rect x="4" y="7.5" width="16" height="1.5"></rect><rect x="4" y="15" width="16" height="1.5"></rect></svg></button>
+				<div class="wp-block-navigation__responsive-container" id="modal-3" data-wp-class--has-modal-open="state.isMenuOpen" data-wp-class--is-menu-open="state.isMenuOpen" data-wp-watch="callbacks.initMenu" data-wp-on--keydown="actions.handleMenuKeydown" data-wp-on-async--focusout="actions.handleMenuFocusout" tabindex="-1">
+					<div class="wp-block-navigation__responsive-close" tabindex="-1">
+						<div class="wp-block-navigation__responsive-dialog" data-wp-bind--aria-modal="state.ariaModal" data-wp-bind--aria-label="state.ariaLabel" data-wp-bind--role="state.roleAttribute">
+							<button aria-label="Close menu" class="wp-block-navigation__responsive-container-close" data-wp-on-async--click="actions.closeMenuOnClick"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="m13.06 12 6.47-6.47-1.06-1.06L12 10.94 5.53 4.47 4.47 5.53 10.94 12l-6.47 6.47 1.06 1.06L12 13.06l6.47 6.47 1.06-1.06L13.06 12Z"></path></svg></button>
+							<div class="wp-block-navigation__responsive-container-content" data-wp-watch="callbacks.focusFirstElement" id="modal-3-content">
+								<ul class="wp-block-navigation__container is-responsive items-justified-right wp-block-navigation"><ul class="wp-block-page-list"><li class="wp-block-pages-list__item wp-block-navigation-item open-on-hover-click"><a class="wp-block-pages-list__item__link wp-block-navigation-item__content" href="https://mpressivewebdv.com/wpp/login-customizer/">Login Customizer</a></li><li class="wp-block-pages-list__item wp-block-navigation-item open-on-hover-click"><a class="wp-block-pages-list__item__link wp-block-navigation-item__content" href="https://mpressivewebdv.com/wpp/sample-page/">Sample Page</a></li></ul></ul>
+							</div>
+						</div>
+					</div>
+				</div></nav>
+							</div>
+							
+						</div>
+						
+					</div>
+					
+</footer>
