7e298daff0e05cc866f4cfcf6d9b792c52cbe8c9
Michaela Presser <147770729+michaelamej@users.noreply.github.com>
2025-01-13 09:40:59 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,52 @@
+<header class="wp-block-template-part">
+
+					<div class="wp-block-group alignfull is-layout-constrained wp-block-group-is-layout-constrained">
+					
+					<div class="wp-block-group alignfull wp-block-main-header is-layout-constrained wp-block-group-is-layout-constrained">
+						
+						<div class="wp-block-group alignwide is-content-justification-space-between is-layout-flex wp-container-core-group-is-layout-4 wp-block-group-is-layout-flex" style="padding-top:var(--wp--custom--spacing--extra-small);padding-bottom:var(--wp--custom--spacing--extra-small)">
+							
+							<div class="wp-block-group is-layout-flex wp-block-group-is-layout-flex">
+								
+								
+								<div class="wp-block-group is-layout-flow wp-block-group-is-layout-flow"><h1 class="wp-block-site-title"><a href="https://mpressivewebdv.com/wpp" target="_self" rel="home" aria-current="page">Web Design</a></h1>
+
+								<p style="margin-top:4px;" class="wp-block-site-tagline">Web Design Homepage</p></div>
+								
+
+							</div>
+							
+
+							<nav class="is-responsive items-justified-right wp-block-navigation is-content-justification-right is-layout-flex wp-container-core-navigation-is-layout-1 wp-block-navigation-is-layout-flex" aria-label="" data-wp-interactive="core/navigation" data-wp-context="{&quot;overlayOpenedBy&quot;:{&quot;click&quot;:false,&quot;hover&quot;:false,&quot;focus&quot;:false},&quot;type&quot;:&quot;overlay&quot;,&quot;roleAttribute&quot;:&quot;&quot;,&quot;ariaLabel&quot;:&quot;Menu&quot;}"><button aria-haspopup="dialog" aria-label="Open menu" class="wp-block-navigation__responsive-container-open " data-wp-on-async--click="actions.openMenuOnClick" data-wp-on--keydown="actions.handleMenuKeydown"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><rect x="4" y="7.5" width="16" height="1.5"></rect><rect x="4" y="15" width="16" height="1.5"></rect></svg></button>
+				<div class="wp-block-navigation__responsive-container" id="modal-1" data-wp-class--has-modal-open="state.isMenuOpen" data-wp-class--is-menu-open="state.isMenuOpen" data-wp-watch="callbacks.initMenu" data-wp-on--keydown="actions.handleMenuKeydown" data-wp-on-async--focusout="actions.handleMenuFocusout" tabindex="-1">
+					<div class="wp-block-navigation__responsive-close" tabindex="-1">
+						<div class="wp-block-navigation__responsive-dialog" data-wp-bind--aria-modal="state.ariaModal" data-wp-bind--aria-label="state.ariaLabel" data-wp-bind--role="state.roleAttribute">
+							<button aria-label="Close menu" class="wp-block-navigation__responsive-container-close" data-wp-on-async--click="actions.closeMenuOnClick"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="m13.06 12 6.47-6.47-1.06-1.06L12 10.94 5.53 4.47 4.47 5.53 10.94 12l-6.47 6.47 1.06 1.06L12 13.06l6.47 6.47 1.06-1.06L13.06 12Z"></path></svg></button>
+							<div class="wp-block-navigation__responsive-container-content" data-wp-watch="callbacks.focusFirstElement" id="modal-1-content">
+								<ul class="wp-block-navigation__container is-responsive items-justified-right wp-block-navigation"><ul class="wp-block-page-list"><li class="wp-block-pages-list__item wp-block-navigation-item open-on-hover-click"><a class="wp-block-pages-list__item__link wp-block-navigation-item__content" href="https://mpressivewebdv.com/wpp/login-customizer/">Login Customizer</a></li><li class="wp-block-pages-list__item wp-block-navigation-item open-on-hover-click"><a class="wp-block-pages-list__item__link wp-block-navigation-item__content" href="https://mpressivewebdv.com/wpp/sample-page/">Sample Page</a></li></ul></ul>
+							</div>
+						</div>
+					</div>
+				</div></nav>
+
+							
+							<div class="wp-block-group is-layout-flex wp-block-group-is-layout-flex">
+								
+								<div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
+									
+									<div class="wp-block-button is-style-catch-fse-button"><a class="wp-block-button__link">Log in</a></div>
+									
+
+									
+									<div class="wp-block-button is-style-outline is-style-outline--2"><a class="wp-block-button__link">Get Started</a></div>
+									
+								</div>
+								
+							</div>
+							
+						</div>
+						
+					</div>
+					</div>
+					
+</header>
